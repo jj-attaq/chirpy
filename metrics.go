698c8485ba2f2feaa5899111ad2ea76a65fb29c6
jj-attaq <jj.attaq@gmail.com>
2025-09-08 14:21:43 +0200

    validateJSON function added

--- metrics.go
+++ metrics.go
@@ -9,13 +9,13 @@ func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(fmt.Sprintf(`
-			<html>
-			  <body>
-				<h1>Welcome, Chirpy Admin</h1>
-				<p>Chirpy has been visited %d times!</p>
-			  </body>
-			</html>
-		`, cfg.fileserverHits.Load())))
+<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>
+	`, cfg.fileserverHits.Load())))
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
