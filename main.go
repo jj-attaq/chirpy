e611d0bf63aa0edc75d6edb7713a3b25066e6995
jj-attaq <jj.attaq@gmail.com>
2025-09-08 18:33:44 +0200

    cleaned up, bootdev solution much much better

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 
 	"net/http"
@@ -22,12 +21,13 @@ func main() {
 	srvMux := http.NewServeMux()
 
 	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
-
 	srvMux.Handle("/app/", fsHandler)
+
 	srvMux.HandleFunc("GET /api/healthz", handlerReadiness)
+	srvMux.HandleFunc("POST /api/validate_chirp", handlerChirpsValidate)
+
 	srvMux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	srvMux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
-	srvMux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -36,63 +36,4 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
-}
-
-func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
-	type body struct {
-		Body string
-	}
-
-	type errBody struct {
-		Error string `json:"error"`
-	}
-
-	type validBody struct {
-		Valid bool `json:"valid"`
-	}
-
-	decoder := json.NewDecoder(r.Body)
-	reqBody := body{}
-
-	err := decoder.Decode(&reqBody)
-	if err != nil {
-		log.Printf("%s", err)
-		// w.WriteHeader(500)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	valid := validBody{
-		Valid: true,
-	}
-	validJSON, err := json.Marshal(valid)
-	if err != nil {
-		newErr := errBody{
-			Error: "something went wrong",
-		}
-		errJSON, err := json.Marshal(newErr)
-		if err != nil {
-			log.Printf("Error marshalling Error JSON: %s", err)
-		}
-		log.Printf("Error marshalling JSON: %s", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(errJSON))
-		return
-	}
-
-	if len(reqBody.Body) > 140 {
-		newErr := errBody{
-			Error: "Chirp is too long",
-		}
-		invalidLengthJSON, err := json.Marshal(newErr)
-		if err != nil {
-			log.Printf("Error marshalling Error JSON: %s", err)
-		}
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(invalidLengthJSON))
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	w.Write(validJSON)
 }
