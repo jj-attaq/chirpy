eed7e1edb1c02920337c74e462f2dbffc70caa71
jj-attaq <jj.attaq@gmail.com>
2025-09-10 13:59:03 +0200

    refactored

--- handler_users_create.go
+++ handler_users_create.go
@@ -8,10 +8,6 @@ import (
 	"github.com/google/uuid"
 )
 
-type emailReq struct {
-	Email string `json:"email"`
-}
-
 type User struct {
 	ID        uuid.UUID `json:"id"`
 	CreatedAt time.Time `json:"created_at"`
@@ -20,26 +16,35 @@ type User struct {
 }
 
 func (cfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
-	decoder := json.NewDecoder(r.Body)
-	email := emailReq{}
+	type parameters struct {
+		Email string `json:"email"`
+	}
+	type response struct {
+		User
+	}
 
-	err := decoder.Decode(&email)
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+
+	err := decoder.Decode(&params)
 	if err != nil {
 		respondWithError(w, http.StatusBadRequest, "Couldn't decode parameters", err)
 		return
 	}
 
 	// user requests context for timeout
-	user, err := cfg.db.CreateUser(r.Context(), email.Email)
+	user, err := cfg.db.CreateUser(r.Context(), params.Email)
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "", err)
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create user", err)
 		return
 	}
 
-	respondWithJSON(w, http.StatusCreated, User{
-		ID:        user.ID,
-		CreatedAt: user.CreatedAt,
-		UpdatedAt: user.UpdatedAt,
-		Email:     user.Email,
+	respondWithJSON(w, http.StatusCreated, response{
+		User: User{
+			ID:        user.ID,
+			CreatedAt: user.CreatedAt,
+			UpdatedAt: user.UpdatedAt,
+			Email:     user.Email,
+		},
 	})
 }
--- reset.go
+++ reset.go
@@ -1,22 +1,28 @@
 package main
 
 import (
-	"log"
 	"net/http"
 )
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
 	if cfg.platform != "dev" {
-		w.Header().Set("Content-Type", "text/plain; charset=UTF-8")
 		w.WriteHeader(http.StatusForbidden)
+		w.Write([]byte("Reset is only allowed in dev environment."))
 		return
 	}
 
+	cfg.fileserverHits.Store(0)
+
 	if err := cfg.db.ResetDb(r.Context()); err != nil {
-		log.Panic(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Failed to reset the database: " + err.Error()))
+		return
 	}
 
-	w.Header().Set("Content-Type", "text/plain; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
-	cfg.fileserverHits.Add(-cfg.fileserverHits.Load())
+	w.Write([]byte("Hits reset to 0 and database reset to initial state."))
+
+	// w.Header().Set("Content-Type", "text/plain; charset=UTF-8")
+	// w.WriteHeader(http.StatusOK)
+	// cfg.fileserverHits.Add(-cfg.fileserverHits.Load())
 }
